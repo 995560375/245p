6680ad05cee447be88445273f7bc7b3e6b92f224
soap-cpp <995560375@qq.com>
2022-11-02 11:00:38 -0700

    ex8 bug fix

--- exercise8/client/src/App.js
+++ exercise8/client/src/App.js
@@ -7,12 +7,14 @@ import Header from "./Header";
 import Footer from "./Footer";
 import Sidebar from "./Sidebar";
 import Main from './Main';
+import SidebarAndMain from './SidebarAndMain';
 function App() {
   return (
     <div className="App">
       <Header></Header>
-      <Sidebar></Sidebar>
-      <Main></Main>
+      {/* <Sidebar></Sidebar>
+      <Main></Main> */}
+      <SidebarAndMain></SidebarAndMain>
       <Footer></Footer>
     </div>
   );
---
+++ exercise8/client/src/SidebarAndMain.js
@@ -0,0 +1,17 @@
+// import "./Sidebar.css";
+
+import Sidebar from "./Sidebar"
+import Main from "./Main";
+
+function SidebarAndMain() {
+    return (
+        
+          <div className="sidebarandmain">
+            <Sidebar></Sidebar>
+            <Main></Main>
+          </div>
+        
+      );
+}
+
+export default SidebarAndMain;
